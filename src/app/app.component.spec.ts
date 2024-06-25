debfbd17ef6cdb285c01d79c8e98ae066e6702d8
lbuettrich <info@leonbuettrich.de>
2024-06-26 00:55:37 +0200

    - Adjusted naming

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'component-resolver-demo' title`, () => {
+  it(`should have the 'ng-lazy-feature-loader' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual("component-resolver-demo");
+    expect(app.title).toEqual("ng-lazy-feature-loader");
   });
 
   it("should render title", () => {
@@ -25,7 +25,7 @@ describe("AppComponent", () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector("h1")?.textContent).toContain(
-      "Hello, component-resolver-demo",
+      "Hello, ng-lazy-feature-loader",
     );
   });
 });
